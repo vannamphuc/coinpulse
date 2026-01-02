9ff9d2f8e78dec6206fc25f9e2c864f0571a8509
dktechprojectphuc <dktechprojectphuc@gmail.com>
2026-01-02 10:16:03 +0700

    Refactor CandlestickChart to convert OHLC data to seconds and update loading state handling; adjust CoinOverview data fetching for improved performance

--- components/CandlestickChart.tsx
+++ components/CandlestickChart.tsx
@@ -27,7 +27,6 @@ const CandlestickChart = ({
   const chartRef = useRef<IChartApi | null>(null);
   const candleSeriesRef = useRef<ISeriesApi<"Candlestick"> | null>(null);
 
-  const [loading, setLoading] = useState(false);
   const [period, setPeriod] = useState(initialPeriod);
   const [ohlcData, setOhlcData] = useState<OHLCData[]>(data ?? []);
   const [isPending, startTransition] = useTransition();
@@ -69,7 +68,18 @@ const CandlestickChart = ({
 
     const series = chart.addSeries(CandlestickSeries, getCandlestickConfig());
 
-    series.setData(convertOHLCData(ohlcData));
+    const convertedToSeconds = ohlcData.map(
+      (item) =>
+        [
+          Math.floor(item[0] / 1000),
+          item[1],
+          item[2],
+          item[3],
+          item[4],
+        ] as OHLCData
+    );
+
+    series.setData(convertOHLCData(convertedToSeconds));
     chart.timeScale().fitContent();
 
     chartRef.current = chart;
@@ -89,7 +99,7 @@ const CandlestickChart = ({
       chartRef.current = null;
       candleSeriesRef.current = null;
     };
-  }, [height]);
+  }, [height, period]);
 
   useEffect(() => {
     if (!candleSeriesRef.current) return;
@@ -127,7 +137,7 @@ const CandlestickChart = ({
                   period === value ? "config-button-active" : "config-button"
                 }
                 onClick={() => handlePeriodChange(value)}
-                disabled={loading}
+                disabled={isPending}
               >
                 {label}
               </button>
